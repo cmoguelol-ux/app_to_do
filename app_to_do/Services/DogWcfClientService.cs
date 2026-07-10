5dc7192b9d9ecf32a1a03668e2f5073ea6fc8b7b
Carlos Arturo Moguel Olvera <carlitosmoguelol@gmail.com>
2026-07-09 21:32:04 -0600

    Conectar MVC con WCF DogServiceReference

--- app_to_do/Services/DogWcfClientService.cs
+++ app_to_do/Services/DogWcfClientService.cs
@@ -1,63 +1,38 @@
-﻿// Hecho por Carlos Arturo Moguel Olvera s
+﻿//Hecho por Carlos Arturo Moguel Olvera
 
 using app_to_do.Models;
-using System.ServiceModel;
 
 namespace app_to_do.Services
 {
     public class DogWcfClientService
     {
-        private const string ServiceUrl = "http://localhost:58751/DogService.svc";
-
         public async Task<DogViewModel> ObtenerPerritoDelDia()
         {
             var model = new DogViewModel();
 
             try
             {
-                var binding = new BasicHttpBinding();
-                var endpoint = new EndpointAddress(ServiceUrl);
+                var client = new DogServiceReference.DogServiceClient();
 
-                var factory = new ChannelFactory<IDogService>(binding, endpoint);
-                IDogService client = factory.CreateChannel();
-
-                string imagenUrl = await Task.Run(() =>
-                    client.ObtenerPerritoDelDia()
-                );
-
-                try
-                {
-                    ((IClientChannel)client).Close();
-                    factory.Close();
-                }
-                catch
-                {
-                    ((IClientChannel)client).Abort();
-                    factory.Abort();
-                }
-
+                string imagenUrl =
+                    await client.ObtenerPerritoDelDiaAsync();
                 if (string.IsNullOrWhiteSpace(imagenUrl))
                 {
-                    model.Mensaje = "No se pudo obtener el perrito del día.";
+                    model.Mensaje = "No se pudo obtener el perrito del dia.";
                     return model;
                 }
 
                 model.ImagenUrl = imagenUrl;
-                model.Mensaje = "Perrito del día recuperado correctamente.";
+                model.Mensaje = "Perrito del dia recuperado correctamente.";
             }
             catch (Exception ex)
             {
-                model.Mensaje = "Ocurrió un error al consumir el servicio WCF: " + ex.Message;
+                model.Mensaje =
+                    "Ocurrio un error al consumir el servicio WCF: "
+                    + ex.Message;
             }
 
             return model;
         }
-    }
-
-    [ServiceContract]
-    public interface IDogService
-    {
-        [OperationContract]
-        string ObtenerPerritoDelDia();
     }
 }
